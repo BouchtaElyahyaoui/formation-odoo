7b16281767a05ffa2715b62aefbf8e233bdd763d
Bouchta Elyahyaoui <bouchtaelyahyaoui2264@gmail.com>
2023-02-19 12:20:07 +0100

    Adding Chatter

--- instance_request/__manifest__.py
+++ instance_request/__manifest__.py
@@ -3,11 +3,13 @@
     'author': 'Bouchta Elyahyaoui',
     'website': 'www.odoo.com',
     'summary': 'This is our fist sumary',
+    'depends': ['mail'],
     'data': [
-        'security/ir.model.access.csv',
         'security/security.xml',
+        'security/ir.model.access.csv',
         'views/instance_request.xml',
         'views/odoo_version.xml',
         'data/data.xml',
+        'data/mail_template.xml'
     ]
 }
--- instance_request/models/instance_request.py
+++ instance_request/models/instance_request.py
@@ -4,11 +4,12 @@ from odoo import api, fields, models
 
 class InstanceRequest(models.Model):
     _name = "instance.request"  # odoo will create a postgres table (hospital_patient
+    _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "All instance requests"
-    name = fields.Char(string='Designation')
+    name = fields.Char(string='Designation',tracking=True)
     address_ip = fields.Char(string='IP Address')
     active = fields.Boolean(string='Actif', default=True)
-    cpu = fields.Char(string='CPU')
+    cpu = fields.Char(string='CPU',)
     ram = fields.Char(string='RAM')
     disk = fields.Char(string='DISK')
     url = fields.Char(string='URL')
@@ -18,10 +19,11 @@ class InstanceRequest(models.Model):
         ('inprogress', 'In Progress'),
         ('done', 'Done')
     ], string='Statut'
-        , default="draft")
-    limit_date = fields.Date(string='Processing Deadline')
+        , default="draft",tracking=True)
+    limit_date = fields.Date(string='Processing Deadline',tracking=True)
     treat_date = fields.Date(string='Processing Date')
     treat_duration = fields.Float(string='Processing Period')
+
 
 
     def action_brouillon(self):
@@ -35,7 +37,15 @@ class InstanceRequest(models.Model):
     def action_en_traitement(self):
         for record in self:
             record.state = 'inprogress'
+            template = self.env.ref("instance_request.instance_request_creation_progress")
+            template.send_mail(record.id,
+                               email_values={'email_to': record.create_uid.email, 'email_from': self.env.user.email})
+
+
 
     def action_traitée(self):
         for record in self:
             record.state = 'done'
+            template = self.env.ref("instance_request.instance_request_creation")
+            template.send_mail(record.id,
+                               email_values={'email_to': record.create_uid.email, 'email_from': self.env.user.email})
